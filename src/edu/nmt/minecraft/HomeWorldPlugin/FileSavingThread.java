6528c4ea0a4382c6e1f5d7328cc5d58fb134d612
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2014-03-13 12:11:41 -0600

    Update FileSavingThread.java

--- src/edu/nmt/minecraft/HomeWorldPlugin/FileSavingThread.java
+++ src/edu/nmt/minecraft/HomeWorldPlugin/FileSavingThread.java
@@ -12,30 +12,15 @@ class FileSavingThread extends Thread {
 	
 	@Override
 	public void run(){
-		World wilderness = Bukkit.getWorld("Wilderness");
-		if (wilderness != null){
-			wilderness.setMonsterSpawnLimit(700);
-			wilderness.setTicksPerMonsterSpawns(10);
-			//wilderness.setTime(15000);
-		}
 
 		while(true){
 			//save everything
 			HomeWorldPlugin.saveAll();
 			for (int i = 0; i < 10; i++){
-				wilderness = Bukkit.getWorld("Wilderness");
 				try {
 					Thread.sleep(1000 * 60);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				}
-				
-				if (wilderness != null){
-					wilderness.setMonsterSpawnLimit(700);
-					wilderness.setTicksPerMonsterSpawns(10);
-					//wilderness.setTime(15000);
-					//wilderness.
-					
 				}
 				
 			}
